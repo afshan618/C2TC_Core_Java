7d6e37e73308cfa9f8e31ae556d0016b60fe088e
Toshiba <Toshiba@192.168.0.2>
2022-07-19 12:06:39 +0530

    assignment

---
+++ AssignmentArray/src/javaprograms/Arrayassg3.java
@@ -0,0 +1,27 @@
+package javaprograms;
+
+import java.util.Scanner;
+
+public class Arrayassg3 {
+	public static void main(String[] args)   
+	{  
+	int size;  
+	Scanner s=new Scanner(System.in);  
+	System.out.print("Enter the size of the Array");  
+	size=s.nextInt();  
+	int[] array = new int[10];  
+
+		System.out.println("Enter the elements of the array: ");  
+		for(int i=0; i<size; i++)  
+		{  
+
+				array[i]=s.nextInt();  
+		}  
+
+	 
+		for (int i=0; i<size; i++)   
+		{  
+				System.out.println(array[i]);  
+		}	  
+	}  
+}
