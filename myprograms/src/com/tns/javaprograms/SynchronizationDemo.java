ff0f425c0bc51058872748edd78b66eaae77ab91
Toshiba <Toshiba@192.168.0.3>
2022-07-22 13:15:29 +0530

    Synchronization

---
+++ myprograms/src/com/tns/javaprograms/SynchronizationDemo.java
@@ -0,0 +1,52 @@
+//Demo on synchronized
+package com.tns.javaprograms;
+
+
+
+class Counter
+	{
+		int count;
+		public void inc()
+		{
+			count++;
+			
+		}
+	}
+	public class SynchronizationDemo {
+		public static void main (String[]args) throws InterruptedException{
+	Counter c=new Counter();
+		Thread  t1=new Thread(new Runnable()
+				{
+			public void run() {
+				for (int i=1;i<=10000;i++)
+				{
+					c.inc();
+					
+				}
+			}
+				});
+		//second thread
+		Thread  t2=new Thread(new Runnable()
+		{
+	public void run() {
+		for (int i=1;i<=10000;i++)
+		{
+			c.inc();
+			
+		}
+	}
+		});
+		t1.start();
+		t2.start();
+		t1.join();
+		t2.join();
+		System.out.println(c.count);
+		}
+	}
+	
+		
+		
+		
+		
+		
+		
