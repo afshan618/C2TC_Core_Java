c7af7da2374816c5df55434fa8230d0ee0f95f63
Toshiba <Toshiba@Toshiba-PC>
2022-07-29 19:45:57 +0530

    Autoboxing

---
+++ myprograms/src/Wrapper/Autoboxing.java
@@ -0,0 +1,12 @@
+package Wrapper;
+
+public class Autoboxing {
+	public static void main(String[] args) {
+		int a = 20;
+		Integer i =a;
+		Integer j = new Integer(10);
+		System.out.println(a+" " +j + " " +j);
+		System.out.println(i);
+	}
+
+}
