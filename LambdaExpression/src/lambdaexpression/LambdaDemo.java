450b34e474996a48e72e2e628ee3b8fbea9cc05c
Toshiba <Toshiba@Toshiba-PC>
2022-08-03 15:30:21 +0530

    Lambdaexpression

---
+++ LambdaExpression/src/lambdaexpression/LambdaDemo.java
@@ -0,0 +1,20 @@
+package lambdaexpression;
+
+	interface Anonymous
+	{
+		void show();
+	}
+
+	public class LambdaDemo {
+
+		public static void main(String[] args) {
+			Anonymous l=() -> 
+			{
+				System.out.println("Lambda Demo for simplification");
+			};
+			l.show();
+
+		}
+
+	}
+
