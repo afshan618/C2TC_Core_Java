75e156858ea979b5da64bc1a86df34b2b0ca5c70
Toshiba <Toshiba@Toshiba-PC>
2022-08-10 14:19:51 +0530

    my programs

---
+++ myprograms/src/junittest/TreeSetDemo.java
@@ -0,0 +1,40 @@
+package junittest;
+
+
+import java.util.*;
+
+class TreeSetDemo {
+	public static void main(String args[]) 
+	
+	{	TreeSet<Comparable> treeSet1=new TreeSet<Comparable>();
+		treeSet1.add(56);
+		treeSet1.add(62);
+		treeSet1.add(50);
+		treeSet1.add(10);
+		treeSet1.add(15);
+		
+	
+		
+		//Ascending order
+		System.out.println(treeSet1);
+	
+		// remove()
+		// clone()
+	
+//		TreeSet<String> treeSet2 = new TreeSet<String>(); 
+//
+//		treeSet2.add("One");
+//		treeSet2.add("Two");
+//		treeSet2.add("Three");
+//		treeSet2.add("Four");
+//		treeSet2.add("Five");
+//
+//		System.out.println("Contents of TreeSet");
+//		//cursor 
+//		Iterator iterator = treeSet2.iterator(); // obtaining iterator object
+//		while (iterator.hasNext()) { // to iterate through collection.
+//			Object object = iterator.next();
+//			System.out.print(object + "\t");
+//		}
+	}
+}
