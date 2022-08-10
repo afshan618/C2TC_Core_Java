f229d9f481b08da46ebc03395864214de616fe3f
Toshiba <Toshiba@Toshiba-PC>
2022-08-10 14:05:43 +0530

    collection framework

---
+++ myprograms/src/collectionframework/NameComparator.java
@@ -0,0 +1,20 @@
+package collectionframework;
+
+
+	import java.util.Comparator;
+
+	class NameComparator implements Comparator<Employee>
+	{
+
+		@Override
+		public int compare(Employee emp1,Employee emp2) {
+			String emp1Name = ((Employee) emp1).getName();
+			String emp2Name = ((Employee) emp2).getName();
+	//comparing Strings
+			return emp1Name.compareTo(emp2Name);
+			
+
+		}
+		
+
+}
